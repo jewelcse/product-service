45e809599ae6112e3d35478c4b78c9cadf4067a5
jewelcsebu <jewelchowdhurybucse@gmail.com>
2021-11-06 10:53:25 +0600

    added cros configuration

--- src/main/java/com/productservice/ProductServiceApplication.java
+++ src/main/java/com/productservice/ProductServiceApplication.java
@@ -2,53 +2,11 @@ package com.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
-//@EnableCaching
-
 public class ProductServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductServiceApplication.class, args);
 	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedMethods("*")
-						.allowedOrigins("http://localhost:3000");
-			}
-		};
-	}
-
-//	@Bean
-//	public CorsFilter corsFilter() {
-//		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//		final CorsConfiguration config = new CorsConfiguration();
-//		//config.setAllowCredentials(true);
-//		config.addAllowedOrigin("*"); // this allows all origin
-//		config.addAllowedHeader("*"); // this allows all headers
-//		config.addAllowedMethod("OPTIONS");
-//		config.addAllowedMethod("HEAD");
-//		config.addAllowedMethod("GET");
-//		config.addAllowedMethod("PUT");
-//		config.addAllowedMethod("POST");
-//		config.addAllowedMethod("DELETE");
-//		config.addAllowedMethod("PATCH");
-//		source.registerCorsConfiguration("/**", config);
-//		return new CorsFilter(source);
-//	}
-
 }
